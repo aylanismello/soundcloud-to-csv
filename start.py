a6c07be73af96bb8ae6b12528da099495338d01c
Aylan Mello <aylanismello@gmail.com>
2018-10-01 17:32:37 -0700

    extended functionality to accept any file name

--- start.py
+++ start.py
@@ -1,45 +1,45 @@
 import soundcloud
+import sys
 import pdb
 import re
 import csv
 
 client = soundcloud.Client(client_id='caf73ef1e709f839664ab82bef40fa96')
 
+
+try:
+    file_name = sys.argv[1]
+except:
+    raise Exception('Please provide file name')
+
+
 handle_urls = {
-    'https://soundcloud.com/user-733227616': {},
-    'https://soundcloud.com/k_sadilla': {},
-    'https://soundcloud.com/thunderstone-radio': {},
-    'https://soundcloud.com/user-658289159': {},
-    'https://soundcloud.com/user-441287236': {},
-    'https://soundcloud.com/my-big-dill': {}
+    'https://soundcloud.com/umdotdot': {}
 }
 
 handle_names = {
-    'https://soundcloud.com/user-733227616': 'Drty Fxn Svge',
-    'https://soundcloud.com/k_sadilla': 'K Sadilla',
-    'https://soundcloud.com/thunderstone-radio': 'Thunderstone Radio ',
-    'https://soundcloud.com/user-658289159': 'Dre big rxx',
-    'https://soundcloud.com/user-441287236': 'Last time you rolled',
-    'https://soundcloud.com/my-big-dill': 'Molly mcthizzletoes'
+    'https://soundcloud.com/umdotdot': 'Um'
 }
-
-# receives user collection (LIST)
-#  { 'swinga': ['first', 'second']}
 def add_users(user_collection):
     users = {}
+    user_data = { 'emails': [], 'youtube': None }
 
     for user in user_collection:
         emails = re.findall(r'[\w\.-]+@[\w\.-]+', user.description or '')
         if emails:
-            users[user.username] = emails
-        else:
-            emails = []
-            web_profiles = client.get(f'/users/{user.id}/web-profiles')
-            for web_profile in web_profiles:
-                if web_profile.service == 'email':
-                    emails.append(web_profile.url)
-            if len(emails):
-                users[user.username] = emails
+            # users[user.username] = emails
+            user_data['emails'] = emails
+            users[user.username] = user_data
+        # else:
+        #     emails = []
+        #     web_profiles = client.get(f'/users/{user.id}/web-profiles')
+        #     for web_profile in web_profiles:
+        #         if web_profile.service == 'email':
+        #             emails.append(web_profile.url)
+        #     if len(emails):
+        #         user_data['emails']= emails
+        #         users[user.username] = user_data
+                # users[user.username] = emails
     return users
 
 for url, users in handle_urls.items():
@@ -55,11 +55,11 @@ for url, users in handle_urls.items():
     handle_urls[url] = users
 
 
-with open('thunderstone_emails.csv', mode='w') as csv_file:
+with open(f'./data/{file_name}.csv', mode='w') as csv_file:
     fieldnames = ['root_handle', 'handle', 'emails']
     writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
 
     writer.writeheader()
     for url, users in handle_urls.items():
-        for handle, emails in users.items():
-            writer.writerow({'root_handle': handle_names[url], 'handle': handle, 'emails': ', '.join(emails)})
+        for handle, user_data in users.items():
+            writer.writerow({'root_handle': handle_names[url], 'handle': handle, 'emails': ', '.join(user_data['emails'])})
