c2c02a723f937fbf75ca2cfa41925d1890e5cafa
Aylan Mello <aylanismello@gmail.com>
2018-10-01 17:56:38 -0700

    refactored to print youtube as well

--- start.py
+++ start.py
@@ -14,32 +14,36 @@ except:
 
 
 handle_urls = {
-    'https://soundcloud.com/umdotdot': {}
+    'https://soundcloud.com/burncartel': {},
+    'https://soundcloud.com/hoodedyouth': {}
 }
 
 handle_names = {
-    'https://soundcloud.com/umdotdot': 'Um'
+    'https://soundcloud.com/burncartel': 'Burn Cartel',
+    'https://soundcloud.com/hoodedyouth': 'Hooded Youth'
 }
 def add_users(user_collection):
     users = {}
-    user_data = { 'emails': [], 'youtube': None }
 
     for user in user_collection:
+        user_data = { 'emails': [], 'youtube': '' }
+        # find emails in description
         emails = re.findall(r'[\w\.-]+@[\w\.-]+', user.description or '')
         if emails:
-            # users[user.username] = emails
             user_data['emails'] = emails
-            users[user.username] = user_data
-        # else:
-        #     emails = []
-        #     web_profiles = client.get(f'/users/{user.id}/web-profiles')
-        #     for web_profile in web_profiles:
-        #         if web_profile.service == 'email':
-        #             emails.append(web_profile.url)
-        #     if len(emails):
-        #         user_data['emails']= emails
-        #         users[user.username] = user_data
-                # users[user.username] = emails
+
+        # yank webprofiles one by one
+        web_profiles = client.get(f'/users/{user.id}/web-profiles')
+        for web_profile in web_profiles:
+            if web_profile.service == 'email':
+                user_data['emails'].append(web_profile.url)
+            if web_profile.service == 'youtube' and not user_data['youtube']:
+                # pdb.set_trace()
+                user_data['youtube'] = (web_profile.url)
+
+
+        users[user.username] = user_data
+
     return users
 
 for url, users in handle_urls.items():
@@ -56,10 +60,10 @@ for url, users in handle_urls.items():
 
 
 with open(f'./data/{file_name}.csv', mode='w') as csv_file:
-    fieldnames = ['root_handle', 'handle', 'emails']
+    fieldnames = ['root_handle', 'handle', 'emails', 'youtube']
     writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
 
     writer.writeheader()
     for url, users in handle_urls.items():
         for handle, user_data in users.items():
-            writer.writerow({'root_handle': handle_names[url], 'handle': handle, 'emails': ', '.join(user_data['emails'])})
+            writer.writerow({'root_handle': handle_names[url], 'handle': handle, 'emails': ', '.join(user_data['emails']), 'youtube': user_data['youtube'] })
