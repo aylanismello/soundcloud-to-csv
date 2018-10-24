039b995e28a8d73eb541bbe5b0725aa2bfbfb134
Aylan Mello <aylanismello@gmail.com>
2018-10-24 16:20:53 -0600

    changes to read handles from start .json file

---
+++ README.md
@@ -0,0 +1,15 @@
+This project takes in our handles.json and write out a 'lead list' of all the
+users the soundcloud handle in question is following.
+
+
+This project uses pipenv.
+
+GETTING STARTED:
+https://pipenv.readthedocs.io/en/latest/install/#installing-pipenv
+
+
+starting:
+```
+  pipenv install
+  pipenv run python main.py f{output_file_name}
+```
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import soundcloud
 import sys
+import json
 import pdb
 import re
 import csv
@@ -12,21 +13,35 @@ try:
 except:
     raise Exception('Please provide file name')
 
+with open('./handles.json') as f:
+    handles = json.load(f)['handles']
 
-handle_urls = {
-    'https://soundcloud.com/burncartel': {},
-    'https://soundcloud.com/hoodedyouth': {}
-}
 
-handle_names = {
-    'https://soundcloud.com/burncartel': 'Burn Cartel',
-    'https://soundcloud.com/hoodedyouth': 'Hooded Youth'
-}
+handle_urls = {}
+handle_names = {}
+
+for url, handle_name in handles.items():
+    handle_urls[url] = {}
+    handle_names[url] = handle_name
+
+pdb.set_trace()
+
+
 def add_users(user_collection):
     users = {}
 
     for user in user_collection:
-        user_data = { 'emails': [], 'youtube': '' }
+
+        user_data = {
+            'emails': [],
+            'youtube': '',
+            'instagram': '',
+            'facebook': '',
+            'twitter': '',
+            'city': user.city,
+            'country': user.country,
+            'followers_count': user.followers_count
+        }
         # find emails in description
         emails = re.findall(r'[\w\.-]+@[\w\.-]+', user.description or '')
         if emails:
@@ -34,19 +49,29 @@ def add_users(user_collection):
 
         # yank webprofiles one by one
         web_profiles = client.get(f'/users/{user.id}/web-profiles')
+        print(f'fetching web profile for {user.username}')
         for web_profile in web_profiles:
             if web_profile.service == 'email':
                 user_data['emails'].append(web_profile.url)
             if web_profile.service == 'youtube' and not user_data['youtube']:
-                # pdb.set_trace()
                 user_data['youtube'] = (web_profile.url)
+            if web_profile.service == 'twitter' and not user_data['twitter']:
+                user_data['twitter'] = (web_profile.url)
+            if web_profile.service == 'instagram' and not user_data['instagram']:
+                user_data['instagram'] = (web_profile.url)
+            if web_profile.service == 'facebook' and not user_data['facebook']:
+                user_data['facebook'] = (web_profile.url)
 
 
         users[user.username] = user_data
 
     return users
 
+count = 0
+
 for url, users in handle_urls.items():
+    count += 1
+    print(f'on user {count}')
     user_id = client.get('/resolve', url=url).id
     followings = client.get(f'/users/{user_id}/followings', limit=200)
     users = {**users, **add_users(followings.collection)}
